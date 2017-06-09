59dd00cd6eb878621d6dd2d7b2f5bd9437de828b
Kevin Foster <kevin.foster.uk@gmail.com>
2017-06-09 21:10:58 +0100

    Add comments

--- src/schema/util.js
+++ src/schema/util.js
@@ -43,6 +43,13 @@ class SchemaUtil
   {
     return (typeof fieldName == 'string' && fieldName.charAt(0) == '$');
   }
+  // When validating a query its assumed that all fields-names of the query object, other than operators, are data object paths
+  // - operators are skipped and their values are passed to the validator for validation
+  // - this is simple solution that allows validation of schema paths within simple query operators ($and / $or)
+  // - Some operators cannot be validated in this way because the operator values are not schema paths
+  // - One example of this is the $near operator whos value is a specific structure unrelated to the schema 
+  // - As a temporary solution we simply dont attempt to validate operator values which dont fit the basic pattern ($and / $or)
+  // Whats needed is a more intelligent query validator that is aware of how to handle each operator value
   static canValidateQueryOperator(fieldName)
   {
     var cantValidateOperators = ['$near'];
