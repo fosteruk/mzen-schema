63fadf4515d13caac4a5489d92fa18423d36241c
Kevin Foster <kevin.foster.uk@gmail.com>
2017-09-26 15:43:24 +0100

    Expose libs via main script

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = require('./schema');
+module.exports.TypeCaster = require('./type-caster');
+module.exports.Types = require('./schema/types');
+module.exports.Util = require('./schema/util');
