0aca9ed84c149a0b89f3d84dbbf94b007f167884
Kevin Foster <kevin.foster.uk@gmail.co>
2018-04-05 12:01:48 +0100

    Refactor index

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 module.exports = require('./schema');
 module.exports.TypeCaster = require('./type-caster');
 module.exports.ObjectPathAccessor = require('./object-path-accessor');
+module.exports.Validator = require('./schema/validator');
 module.exports.Types = require('./schema/types');
 module.exports.Util = require('./schema/util');
