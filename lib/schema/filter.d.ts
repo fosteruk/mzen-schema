91fb5ebd8d358f0ebdd25cd7244679907f010e95
Kevin Foster <kevin.foster.uk@gmail.com>
2019-03-16 11:25:48 +0800

    Dont store build output in repo

--- lib/schema/filter.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Filter {
-    static filter(value: any, filtersConfig: any): Promise<any>;
-    static isNull(value: any): boolean;
-    static defaultValue(value: any, options: any): any;
-    static trim(value: any): any;
-    static uppercase(value: any): any;
-    static lowercase(value: any): any;
-    static custom(value: any, options: any): any;
-}
--- lib/schema/util.d.ts
+++
@@ -1,6 +0,0 @@
-export default class SchemaUtil {
-    static getSpec(path: any, spec: any): any;
-    static isValidFieldName(fieldName: any): boolean;
-    static isQueryOperator(fieldName: any): boolean;
-    static canValidateQueryOperator(fieldName: any): boolean;
-}
