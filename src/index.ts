4f049a4decfa35cdaa42a7b5d39bc16c4a18cdb9
Kevin Foster <kevin.foster.uk@gmail.com>
2019-03-18 17:06:48 +0800

    Expose ShcemaSpec

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Schema } from './schema';
 
 export * from './schema';
+export * from './schema/spec';
 export * from './schema/manager';
-export * from './object-path-accessor';
 export * from './schema/mapper';
 export * from './schema/validator';
 export * from './schema/types';
-export * from './type-caster';
 export * from './schema/filter';
 export * from './schema/util';
+export * from './object-path-accessor';
+export * from './type-caster';
 
 export default Schema;
